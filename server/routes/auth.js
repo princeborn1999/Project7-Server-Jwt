bf42bcebbb9483c9146024e875bf4fe2b00b070e
Prince <princeborn1999@gmail.com>
2022-11-07 17:19:42 +0800

    [+]Joi validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const registerValidation = require("../validation").registerValidation;
+const loginValidation = require("../validation").loginValidation;
 
 router.use((req, res, next) => {
   console.log("A request is coming in to auth.js");
@@ -10,6 +12,14 @@ router.get("/testAPI", (req, res) => {
     message: "Test API is working",
   };
   return res.json(msgObj);
+});
+
+router.post("/register", (req, res) => {
+  console.log("Register!");
+  const { error } = registerValidation(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 });
 
 module.exports = router;
