c6430d3d2d64b3e7c0945cba3c8ea4639a0723be
Prince <princeborn1999@gmail.com>
2022-11-07 18:54:48 +0800

    [+]Register

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const registerValidation = require("../validation").registerValidation;
 const loginValidation = require("../validation").loginValidation;
+const User = require("../models").userModel;
 
 router.use((req, res, next) => {
   console.log("A request is coming in to auth.js");
@@ -14,11 +15,31 @@ router.get("/testAPI", (req, res) => {
   return res.json(msgObj);
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   console.log("Register!");
   const { error } = registerValidation(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
+  }
+  // check if the user exists
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist)
+    return res.status(400).send("Email has already been registered.");
+  // register the user
+  const newUser = new User({
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    role: req.body.role,
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).send({
+      msg: "success",
+      savedObject: savedUser,
+    });
+  } catch (err) {
+    res.status(400).send("User not saved.");
   }
 });
 
