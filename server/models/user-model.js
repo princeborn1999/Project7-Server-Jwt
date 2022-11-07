eab055f6ca938555f83b3a8e1f700cddea941aed
Prince <princeborn1999@gmail.com>
2022-11-07 15:46:59 +0800

    [+]Models

---
+++ server/models/user-model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 6,
+    maxLength: 1024,
+  },
+  role: {
+    type: String,
+    enum: ["student", "instructor"],
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.method.isStudent = function () {
+  return thie.role == "student";
+};
+
+userSchema.methods.isInstructor = function () {
+  return this.role == "instructor";
+};
+
+// mongoose schema middleware
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+  } else {
+    return next();
+  }
+});
+
+userSchema.methods.comparePassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) return cb(err, isMatch);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model("User", userSchema);
