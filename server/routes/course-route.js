5bd50daaf6f1b28da0bc8fb4d99a8b13527007b9
Prince <princeborn1999@gmail.com>
2022-11-09 15:30:06 +0800

    [+]Enroll With React

--- server/routes/course-route.js
+++ server/routes/course-route.js
@@ -30,6 +30,30 @@ router.get("/instructor/:_instructor_id", (req, res) => {
     });
 });
 
+router.get("/findByName/:name", (req, res) => {
+  let { name } = req.params;
+  Course.find({ title: name })
+    .populate("instructor", ["username", "email"])
+    .then((course) => {
+      res.status(200).send(course);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+router.get("/student/:_student_id", (req, res) => {
+  let { _student_id } = req.params;
+  Course.find({ students: _student_id })
+    .populate("instructor", ["username", "email"])
+    .then((course) => {
+      res.status(200).send(courses);
+    })
+    .catch(() => {
+      res.status(500).send("Cannot get data.");
+    });
+});
+
 router.get("/:_id", (req, res) => {
   let { _id } = req.params;
   Course.findOne({ _id })
@@ -67,6 +91,18 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post("/enroll/:_id", async (req, res) => {
+  let { _id } = req.params;
+  let { user_id } = req.body;
+  try {
+    let course = await Course.findOne({ _id });
+    course.students.push(user_id);
+    await course.save();
+    res.send("Done Enrollment");
+  } catch (err) {
+    res.send(err);
+  }
+});
 router.patch("/:_id", async (req, res) => {
   // validate the inputs before making a new course
   const { error } = courseValidation(req.body);
